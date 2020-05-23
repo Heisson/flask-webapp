11835deafd0bc703fddcb1b0f6e1334688bab670
heisson <heisson70@gmail.com>
2020-05-23 14:43:39 -0300

    Template inheritance and bootstrap added

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+	{
+		"author": "Heisson",
+		"title": "Post 1",
+		"content": "First post content",
+		"date_postet": "May 23, 2020"
+	},
+	{
+		"author": "Helington",
+		"title": "Post 2",
+		"content": "Second post content",
+		"date_postet": "March 12, 2020"
+	}
+
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-	return "<h1>Hello, world!</h1>"
+	return render_template("home.html", posts=posts)
+
 
 @app.route('/about')
 def about():
-	return "<h1>About Page</h1>"
+	return render_template("about.html", title="About")
 
 
 
