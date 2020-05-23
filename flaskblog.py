daa44d187d46b703cb78e16a4f797add510c0285
heisson <heisson70@gmail.com>
2020-05-23 15:51:06 -0300

    Registration page done

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8cef6e14fc961d3d5d40a11f3d382d20'
 
 posts = [
 	{
@@ -23,11 +26,19 @@ posts = [
 def home():
 	return render_template("home.html", posts=posts)
 
-
 @app.route('/about')
 def about():
 	return render_template("about.html", title="About")
 
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template("register.html", title="Register", form=form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
