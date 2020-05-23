2a9e1ce6d385dfbab103758526c1dd5c1de550cf
heisson <heisson70@gmail.com>
2020-05-23 16:38:28 -0300

    Added log in page

--- flaskblog.py
+++ flaskblog.py
@@ -40,9 +40,18 @@ def register():
 
 	return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
 	form = LoginForm()
+
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash(f"You have been logged in!", 'success')
+			return redirect(url_for('home'))
+
+		else:
+			flash(f"Log in unsuccessful. Please check username ans password", 'danger')
+
 	return render_template("login.html", title="Login", form=form)
 
 
