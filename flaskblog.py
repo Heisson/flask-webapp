9c4690a2835c1bf810070c1b62d4a754ed978d16
heisson <heisson70@gmail.com>
2020-05-23 21:02:27 -0300

    Fixed date posted

--- flaskblog.py
+++ flaskblog.py
@@ -10,18 +10,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = None
+
+
+
 posts = [
 	{
 		"author": "Heisson",
 		"title": "Post 1",
 		"content": "First post content",
-		"date_postet": "May 23, 2020"
+		"date_posted": "May 23, 2020"
 	},
 	{
 		"author": "Helington",
 		"title": "Post 2",
 		"content": "Second post content",
-		"date_postet": "March 12, 2020"
+		"date_posted": "March 12, 2020"
 	}
 
 ]
