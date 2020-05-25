76343eff3b1334c04755684cdf2d60f3a7cddac7
heisson <heisson70@gmail.com>
2020-05-25 16:01:53 -0300

    Added set image and scale image feature

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -71,11 +72,11 @@ class UpdateAccountForm(FlaskForm):
 		validators=[DataRequired(), Email()]
 		)
 
-	password = PasswordField(
-		'Password',
-		validators=[DataRequired()]
+	picture = FileField(
+		'Update profile picture', 
+		validators=[FileAllowed(['jpg', 'png'])]
 		)
-	
+
 	submit = SubmitField('Update')
 
 	def validate_username(self, username):
