8189a4450b196b12d067c85b703aa06a313df047
heisson <heisson70@gmail.com>
2020-05-27 15:40:48 -0300

    Added reset password and fixed some bugs

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -107,3 +107,34 @@ class PostForm(FlaskForm):
 		)
 
 	submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+
+	email = StringField(
+		'Email',
+		validators=[DataRequired(), Email()]
+		)
+
+	submit = SubmitField('Request password reset')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+
+		if user is None:
+			raise ValidationError('There is not account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+
+	password = PasswordField(
+		'Password',
+		validators=[DataRequired()]
+		)
+
+	confirm_password = PasswordField(
+		'Password',
+		validators=[DataRequired(), EqualTo('password')]
+		)
+	
+	submit = SubmitField('Reset password')
